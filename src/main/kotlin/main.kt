197b83e39023901976161d2ec58fa574665b4302
stiletto <telo@mail.ru>
2024-11-09 12:36:16 +0300

    task 03 is done

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,16 @@
 fun main() {
+    var purchasePrice = 15000
+    val isMonthlyPurchases = true
 
+    if (purchasePrice in 1001 .. 10000) {
+        purchasePrice -= 100
+    } else if (purchasePrice > 10000) {
+        purchasePrice -= (purchasePrice * 0.05).toInt()
+    }
+
+    if (isMonthlyPurchases) {
+        purchasePrice -= (purchasePrice * 0.01).toInt()
+    }
+
+    println(purchasePrice)
 }
